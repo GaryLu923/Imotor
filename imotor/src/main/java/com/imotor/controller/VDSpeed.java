ad976679b75df063f02852ca39dec1a60e4bb463
jasonlai1218 <jasonlai1218@gmail.com>
2016-03-10 00:32:50 +0800

    1. add class "DBContorller" , "VDSpeed" , "Weather" 2. ViewContorller      add view  "hello/getVDSpeedDataSet" , "hello/getWeatherDataSet"      add function "getVDSpeedDataSet()" , "getWeatherDataSet()" 3. pom.xml      add json 	 add gson 	 add postgresql 	 add log4j

---
+++ imotor/src/main/java/com/imotor/controller/VDSpeed.java
@@ -0,0 +1,86 @@
+package com.imotor.controller;
+/**
+ * @author jason.lai
+ * 
+ */
+public class VDSpeed {
+	
+	private String id;
+	private String sectionid;
+	private String sectionname;
+	private String avgspd;
+	private String avgocc;
+	private String totalvol;
+	private String moelevel;
+	private String startwgsx;
+	private String startwgsy;
+	private String endwgsx;
+	private String endwgsy;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSectionid() {
+		return sectionid;
+	}
+	public void setSectionid(String sectionid) {
+		this.sectionid = sectionid;
+	}
+	public String getSectionname() {
+		return sectionname;
+	}
+	public void setSectionname(String sectionname) {
+		this.sectionname = sectionname;
+	}
+	public String getAvgspd() {
+		return avgspd;
+	}
+	public void setAvgspd(String avgspd) {
+		this.avgspd = avgspd;
+	}
+	public String getAvgocc() {
+		return avgocc;
+	}
+	public void setAvgocc(String avgocc) {
+		this.avgocc = avgocc;
+	}
+	public String getTotalvol() {
+		return totalvol;
+	}
+	public void setTotalvol(String totalvol) {
+		this.totalvol = totalvol;
+	}
+	public String getMoelevel() {
+		return moelevel;
+	}
+	public void setMoelevel(String moelevel) {
+		this.moelevel = moelevel;
+	}
+	public String getStartwgsx() {
+		return startwgsx;
+	}
+	public void setStartwgsx(String startwgsx) {
+		this.startwgsx = startwgsx;
+	}
+	public String getStartwgsy() {
+		return startwgsy;
+	}
+	public void setStartwgsy(String startwgsy) {
+		this.startwgsy = startwgsy;
+	}
+	public String getEndwgsx() {
+		return endwgsx;
+	}
+	public void setEndwgsx(String endwgsx) {
+		this.endwgsx = endwgsx;
+	}
+	public String getEndwgsy() {
+		return endwgsy;
+	}
+	public void setEndwgsy(String endwgsy) {
+		this.endwgsy = endwgsy;
+	}
+	
+}
---
+++ imotor/src/main/java/com/imotor/controller/Weather.java
@@ -0,0 +1,188 @@
+package com.imotor.controller;
+/**
+ * @author jason.lai
+ * 
+ */
+public class Weather {
+	
+	private String id;
+	private String region;
+	private String startTime1;
+	private String endTime1;
+	private String pop1;
+	private String startTime2;
+	private String endTime2;
+	private String pop2;
+	private String startTime3;
+	private String endTime3;
+	private String pop3;
+	private String startTime4;
+	private String endTime4;
+	private String pop4;
+	private String startTime5;
+	private String endTime5;
+	private String pop5;
+	private String startTime6;
+	private String endTime6;
+	private String pop6;
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getStartTime1() {
+		return startTime1;
+	}
+
+	public void setStartTime1(String startTime1) {
+		this.startTime1 = startTime1;
+	}
+
+	public String getEndTime1() {
+		return endTime1;
+	}
+
+	public void setEndTime1(String endTime1) {
+		this.endTime1 = endTime1;
+	}
+
+	public String getPop1() {
+		return pop1;
+	}
+
+	public void setPop1(String pop1) {
+		this.pop1 = pop1;
+	}
+
+	public String getStartTime2() {
+		return startTime2;
+	}
+
+	public void setStartTime2(String startTime2) {
+		this.startTime2 = startTime2;
+	}
+
+	public String getEndTime2() {
+		return endTime2;
+	}
+
+	public void setEndTime2(String endTime2) {
+		this.endTime2 = endTime2;
+	}
+
+	public String getPop2() {
+		return pop2;
+	}
+
+	public void setPop2(String pop2) {
+		this.pop2 = pop2;
+	}
+
+	public String getStartTime3() {
+		return startTime3;
+	}
+
+	public void setStartTime3(String startTime3) {
+		this.startTime3 = startTime3;
+	}
+
+	public String getEndTime3() {
+		return endTime3;
+	}
+
+	public void setEndTime3(String endTime3) {
+		this.endTime3 = endTime3;
+	}
+
+	public String getPop3() {
+		return pop3;
+	}
+
+	public void setPop3(String pop3) {
+		this.pop3 = pop3;
+	}
+
+	public String getStartTime4() {
+		return startTime4;
+	}
+
+	public void setStartTime4(String startTime4) {
+		this.startTime4 = startTime4;
+	}
+
+	public String getEndTime4() {
+		return endTime4;
+	}
+
+	public void setEndTime4(String endTime4) {
+		this.endTime4 = endTime4;
+	}
+
+	public String getPop4() {
+		return pop4;
+	}
+
+	public void setPop4(String pop4) {
+		this.pop4 = pop4;
+	}
+
+	public String getStartTime5() {
+		return startTime5;
+	}
+
+	public void setStartTime5(String startTime5) {
+		this.startTime5 = startTime5;
+	}
+
+	public String getEndTime5() {
+		return endTime5;
+	}
+
+	public void setEndTime5(String endTime5) {
+		this.endTime5 = endTime5;
+	}
+
+	public String getPop5() {
+		return pop5;
+	}
+
+	public void setPop5(String pop5) {
+		this.pop5 = pop5;
+	}
+
+	public String getStartTime6() {
+		return startTime6;
+	}
+
+	public void setStartTime6(String startTime6) {
+		this.startTime6 = startTime6;
+	}
+
+	public String getEndTime6() {
+		return endTime6;
+	}
+
+	public void setEndTime6(String endTime6) {
+		this.endTime6 = endTime6;
+	}
+
+	public String getPop6() {
+		return pop6;
+	}
+
+	public void setPop6(String pop6) {
+		this.pop6 = pop6;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+}
